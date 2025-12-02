607c464d18d5bf40391894c20bf0cf732870da75
martin-iflap <martin.iflap@gmail.com>
2025-12-02 22:05:17 +0100

    first simple version

--- search.py
+++ search.py
@@ -1,11 +1,70 @@
-def concordance(document):
-    """Generate a concordance dictionary from the input document string"""
-    if type(document) != str:
-        raise ValueError("This function accepts only string inputs!")
-    con = {}
-    for word in document.split():
-        if con.has_key(word):
-            con[word] += 1
-        else:
-            con[word] = 1
-    return con
+class VectorCompare:
+    def magnitude(self, vector):
+        """Compute the magnitude of a vector represented as a dictionary"""
+        if type(vector) != dict:
+            raise ValueError("This function accepts only dictionary inputs!")
+        total = 0
+        for count in vector.values():
+            total += count ** 2
+        return total ** 0.5
+
+    def relation(self, vector1, vector2):
+        """Compute the cosine similarity between two vectors represented as dictionaries"""
+        if type(vector1) != dict or type(vector2) != dict:
+            raise ValueError("This function accepts only dictionary inputs!")
+        relevance = 0
+        top_value = 0
+        for word in vector1.keys():
+            if vector2.get(word):
+                top_value += vector1[word] * vector2[word]
+        if self.magnitude(vector1) * self.magnitude(vector2) != 0:
+            relevance = top_value / (self.magnitude(vector1) * self.magnitude(vector2))
+        return relevance
+
+    def concordance(self, document):
+        """Generate a concordance dictionary from the input document string"""
+        if type(document) != str:
+            raise ValueError("This function accepts only string inputs!")
+        con = {}
+        for word in document.split():
+            if con.get(word):
+                con[word] += 1
+            else:
+                con[word] = 1
+        return con
+
+def main():
+    v = VectorCompare()
+    documents = {
+        0: '''At Scale You Will Hit Every Performance Issue I used to think I knew a bit about performance scalability and how to keep things trucking when you hit large amounts of data Truth is I know diddly squat on the subject since the most I have ever done is read about how its done To understand how I came about realising this you need some background''',
+        1: '''Richard Stallman to visit Australia Im not usually one to promote events and the like unless I feel there is a genuine benefit to be had by attending but this is one stands out Richard M Stallman the guru of Free Software is coming Down Under to hold a talk You can read about him here Open Source Celebrity to visit Australia''',
+        2: '''MySQL Backups Done Easily One thing that comes up a lot on sites like Stackoverflow and the like is how to backup MySQL databases The first answer is usually use mysqldump This is all fine and good till you start to want to dump multiple databases You can do this all in one like using the all databases option however this makes restoring a single database an issue since you have to parse out the parts you want which can be a pain''',
+        3: '''Why You Shouldn't roll your own CAPTCHA At a TechEd I attended a few years ago I was watching a presentation about Security presented by Rocky Heckman read his blog its quite good In it he was talking about security algorithms The part that really stuck with me went like this''',
+        4: '''The Great Benefit of Test Driven Development Nobody Talks About The feeling of productivity because you are writing lots of code Think about that for a moment Ask any developer who wants to develop why they became a developer One of the first things that comes up is I enjoy writing code This is one of the things that I personally enjoy doing Writing code any code especially when its solving my current problem makes me feel productive It makes me feel like Im getting somewhere Its empowering''',
+        5: '''Setting up GIT to use a Subversion SVN style workflow Moving from Subversion SVN to GIT can be a little confusing at first I think the biggest thing I noticed was that GIT doesnt have a specific workflow you have to pick your own Personally I wanted to stick to my Subversion like work-flow with a central server which all my machines would pull and push too Since it took a while to set up I thought I would throw up a blog post on how to do it''',
+        6: '''Why CAPTCHA Never Use Numbers 0 1 5 7 Interestingly this sort of question pops up a lot in my referring search term stats Why CAPTCHAs never use the numbers 0 1 5 7 Its a relativity simple question with a reasonably simple answer Its because each of the above numbers are easy to confuse with a letter See the below''',
+    }
+    index = {
+        0: v.concordance(documents[0].lower()),
+        1: v.concordance(documents[1].lower()),
+        2: v.concordance(documents[2].lower()),
+        3: v.concordance(documents[3].lower()),
+        4: v.concordance(documents[4].lower()),
+        5: v.concordance(documents[5].lower()),
+        6: v.concordance(documents[6].lower()),
+    }
+
+    search_term = input("Enter search term: ").lower()
+    matches = []
+
+    for i in range(len(index)):
+        score = v.relation(v.concordance(search_term), index[i])
+        if score != 0:
+            matches.append((score, documents[i][:100]))
+
+    matches.sort(reverse=True)
+    for i in matches:
+        print(i[0], i[1])
+
+if __name__ == "__main__":
+    main()
